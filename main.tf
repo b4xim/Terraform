ea7156259dbd28aa70f218700030b0388266624b
B4xim <bk6549@srmist.edu.in>
2026-05-26 07:21:09 +0530

    Added Sensitive file

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ resource "local_file" "ex-1"{
     filename = "${path.module}/hello.txt"
     content = "Hello, I am Basim and this is my first Terraform code!"
 }
+
+resource "local_sensitive_file" "ex-2"{
+    filename = "${path.module}/secret.txt"
+    content = "This is a secret file and should not be exposed! - Password: 123456"
+}
