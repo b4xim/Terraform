40dfab0c481705bc67408b807335a0c41d54dca6
B4xim <bk6549@srmist.edu.in>
2026-05-27 10:26:17 +0530

    Main file edited

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 resource "local_file" "ex-1"{
     #filename = "Basic/hello.txt"
-    filename = "${path.module}/hello.txt"
+    filename = "${path.module}/hello${count.index+1}.txt"
     content = "Hello, I am Basim and this is my first Terraform code!"
+    count = 3
 }
 
 resource "local_sensitive_file" "ex-2"{
     filename = "${path.module}/secret.txt"
     content = "This is a secret file and should not be exposed! - Password: 123456"
-}
+} 
