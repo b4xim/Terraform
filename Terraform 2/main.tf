8ab19378ddcb9b9afa48979d3cef3f3325e0783d
B4xim <bk6549@srmist.edu.in>
2026-05-28 12:04:27 +0530

    2nd folder added

---
+++ Terraform 2/main.tf
@@ -0,0 +1,12 @@
+resource "local_file" "ex-1"{
+    #filename = "Basic/hello.txt"
+    filename = "${path.module}/hello${count.index+1}.txt"
+    content = "Hello, I am Basim and this is my first Terraform code! for File ${count.index+1}"
+    count = 2
+}
+
+resource "local_sensitive_file" "ex-2"{
+    filename = "${path.module}/secret${count.index+1}.md"
+    content = "This is a secret file and should not be exposed! - Password: 123456"
+    count = 2
+}
